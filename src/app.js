46aa4f95c645727a4c37a3e83de3ce840d7f54da
Alan Mills <aelonrench@yahoo.com>
2018-04-13 14:49:02 -0400

    Add esLint to app. And config app environment.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
+import Vue from 'vue'
 
-const app = new Vue ({
-    data: {
-        hello: "Hello world"
-    },
-    template: '<div id="app">{{hello}}</div>'
+const app = new Vue({
+  data: {
+    hello: 'Hello world'
+  },
+  template: '<div id="app">{{hello}}</div>'
 })
 
-export { app };
+export { app }
